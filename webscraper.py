377a1660f8201ee5d84c1149d0401c98f4dfac4c
FERNANDO CUNHA <101639298+omega3344@users.noreply.github.com>
2023-05-22 14:39:42 +0100

    webscraper8

--- webscraper.py
+++ webscraper.py
@@ -71,7 +71,10 @@ def send_email(price, msg, total):
 #main
 
 URL = 'http://datahub.ren.pt/pt/eletricidade/mercado/'
-page_text = requests.get(URL, verify = False).text
+try:
+    page_text = requests.get(URL, timeout=2).text
+except requests.exceptions.RequestException:
+    raise Exception('Failed to connect to %s' % URL) from None
 soup = BeautifulSoup(page_text, 'lxml')
 data = soup.find_all('span', class_='center-cell')
 price = float(data[4].text)
