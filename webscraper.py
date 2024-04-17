497216194451135376f4be656830430748760d69
FERNANDO CUNHA <101639298+omega3344@users.noreply.github.com>
2024-04-17 15:01:36 +0100

    webscraper15

--- webscraper.py
+++ webscraper.py
@@ -6,58 +6,51 @@ import smtplib, ssl
 import urllib3
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
 
 
 #Load environment variables
-#load_dotenv()
+load_dotenv()
 try:
-    #ENV_PASSWORD = os.getenv('PASSWORD')
-    #ENV_FROM = os.getenv('FROM')
-    #ENV_TO = os.getenv('TO')
-    ENV_FROM = os.environ['FROM']
-    ENV_PASSWORD = os.environ['PASSWORD']
-    ENV_TO = os.environ['TO']
+    ENV_PASSWORD = os.getenv('PASSWORD')
+    ENV_FROM = os.getenv('FROM')
+    ENV_TO = os.getenv('TO')
+    #ENV_FROM = os.environ['FROM']
+    #ENV_PASSWORD = os.environ['PASSWORD']
+    #ENV_TO = os.environ['TO']
 except KeyError:
     raise KeyError('Token not available!')
 
-class CustomHttpAdapter (requests.adapters.HTTPAdapter):
-    # "Transport adapter" that allows us to use custom ssl_context.
-
-    def __init__(self, ssl_context=None, **kwargs):
-        self.ssl_context = ssl_context
-        super().__init__(**kwargs)
-
-    def init_poolmanager(self, connections, maxsize, block=False):
-        self.poolmanager = urllib3.poolmanager.PoolManager(
-            num_pools=connections, maxsize=maxsize,
-            block=block, ssl_context=self.ssl_context)
-
-
-def get_legacy_session():
-    ctx = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
-    ctx.options |= 0x4  # OP_LEGACY_SERVER_CONNECT
-    session = requests.session()
-    session.mount('https://', CustomHttpAdapter(ctx))
-    return session
-
 #Calculate invoice total
-def calc_total(price):
-    ER = 370
-    PMD = price/1000
-    Desv = 0.004
-    PT = 1.1507
+def calc_total(price_m):
+    ER = 300
+    PMD = price_m/1000
+    TAR = 0.0365
+    Desv = 0.0065
+    PT = 1.1581
     FA = 1.02
     CG = ER*0.005
-    TP = ER*-0.0958
-    POT = 30*0.229
-    
-    total = round(ER*(PMD+Desv)*PT*FA+CG+TP+POT,2)
+    TP = ER*TAR
+    POT = 30*0.2997
+    AV = 2.85
+    DGEG = 0.07
+    IEC = ER*0.001
+
+    if ER > 100:
+        subtotal6 = 100*(PMD+Desv)*PT*FA+100*0.005+100*TAR+AV
+        subtotal23 = ((ER-100)*(PMD+Desv)*PT*FA+(ER-100)*0.005+(ER-100)*TAR+POT)
+    else:
+        subtotal6 = ER*(PMD+Desv)*PT*FA+ER*0.005+ER*-0.0121+AV
+        subtotal23 = POT  
+
+    IVA = subtotal6*0.06+(subtotal23+AV+DGEG+IEC)*0.23
+
+    total = round(subtotal6+subtotal23+IVA+DGEG+IEC,2)
     
     return total
 
 #Notify price via email
-def send_email(price, msg, total):
+def send_email(price_d, price_m, msg, total):
     message = MIMEMultipart("alternative")
     message["Subject"] = msg + str(date.today().strftime("%d-%m-%Y"))
     message["From"] = ENV_FROM
@@ -69,10 +62,11 @@ def send_email(price, msg, total):
         </head>
         <body>
             <p>Olá,<br><br>
-            O atual preço médio mensal da eletricidade no mercado indexado é de  
-            <strong>{price}€/MWh</strong>.
+            O atual preço diário da eletricidade no mercado indexado é de  
+            <strong>{price_d}€/MWh</strong>, enquanto o preço médio do mês é de 
+            <strong>{price_m}€/MWh</strong>.
             <br>
-            Para um consumo médio de 370kW é estimado um total de fatura de 
+            Para um consumo médio de 300kW é estimado um total de fatura de 
             <strong>{total}€</strong>.
             </p>
         </body>
@@ -94,18 +88,19 @@ def send_email(price, msg, total):
 URL = 'https://datahub.ren.pt/pt/eletricidade/mercado/'
 
 try:
-    page_text = get_legacy_session().get(URL, timeout=5).text
+    page_text = requests.get(URL, timeout=5).text
 except requests.exceptions.RequestException:
     raise Exception('Failed to connect to %s' % URL) from None
 
+soup = BeautifulSoup(page_text, 'html.parser')
+data = soup.find_all('strong')
 
-soup = BeautifulSoup(page_text, 'lxml')
-data = soup.find_all('span', class_='center-cell')
-price = float(data[4].text)
+price_d = float(data[0].text[:-5])
+price_m = float(data[3].text[:-5])
 
-if price > 200:
-    msg = 'ATENÇÃO! Preço médio acima de 200€/MWh! - '
+if price_m > 150:
+    msg = 'ATENÇÃO! Preço médio acima de 150€/MWh! - '
 else:
     msg = 'Preço médio do MWh - '
 
-send_email(price, msg, calc_total(price))
+send_email(price_d, price_m, msg, calc_total(price_m))
